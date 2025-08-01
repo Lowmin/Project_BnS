e6036c70032e245f77e726045ea5704b54aefc6d
SB <h1046rmahoro@naver.com>
2025-08-01 15:53:36 +0900

    타겟 가능 인터페이스 추가

---
+++ Source/project_BnS/TargetAble.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TargetAble.h"
+
+// Add default functionality here for any ITargetAble functions that are not pure virtual.
