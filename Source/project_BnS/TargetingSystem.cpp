6f312b368bf046d29fc0c1136f710fcf9c0ac26e
SB <h1046rmahoro@naver.com>
2025-08-05 01:22:06 +0900

    타겟 추적 기능 임시 추가

--- Source/project_BnS/TargetingSystem.cpp
+++ Source/project_BnS/TargetingSystem.cpp
@@ -2,34 +2,65 @@
 
 
 #include "TargetingSystem.h"
+
+#include "BnsController.h"
 #include "CharacterBase.h"
+#include "MainUi.h"
 #include "TargetAble.h"
+#include "Kismet/GameplayStatics.h"
 
 
 // Sets default values for this component's properties
 UTargetingSystem::UTargetingSystem()
 {
 	// ...
-	
-	//TargetSensor = CreateDefaultSubobject<USphereComponent>(TEXT("Sensor");
-	
-
-
-	//SetSphereRadius(400.0f);
-	//SetHiddenInGame(false);
 }
 
 void UTargetingSystem::BeginPlay()
 {
+	Super::BeginPlay();
+
+	// 틱 활성화 
+	PrimaryComponentTick.Target = this;
+	PrimaryComponentTick.bCanEverTick = true;
+	PrimaryComponentTick.SetTickFunctionEnable(true);
+	PrimaryComponentTick.RegisterTickFunction(GetComponentLevel());
+	
 	SetSphereRadius(400);
 	SetHiddenInGame(false);
 
 	OnComponentBeginOverlap.AddDynamic(this, &UTargetingSystem::OnOverlapBegin);
+	OnComponentEndOverlap.AddDynamic(this, &UTargetingSystem::OnOverlapEnd);
+}
+
+void UTargetingSystem::TickComponent(float DeltaTime, ELevelTick TickType,
+	FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	
+	UE_LOG(LogActor, Warning, TEXT("Tick"));
+
+	FVector2D size;
+	GEngine->GameViewport->GetViewportSize(size);
+	
+	UE_LOG(LogActor, Warning, TEXT("Size : %s"), *size.ToString());
+
+	auto con = GetWorld()->GetFirstPlayerController();
+	ABnsController* bcon = Cast<ABnsController>(con);
+
+	FVector pos(2320, 1280, 96);
+	FVector2D screen;
+	UGameplayStatics::ProjectWorldToScreen(con, pos, screen);
+	UE_LOG(LogActor, Warning, TEXT("screen : %s"), *screen.ToString());
+
+	float x = ((screen.X/size.X) - 0.5f) * size.X;
+	float y = ((screen.Y/size.Y) - 0.5f) * size.Y;
+	bcon->MainUi->SetTarget(true, FVector2D(x, y), FVector2D(200, 200));
 }
 
 void UTargetingSystem::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	UE_LOG(LogActor, Warning, TEXT("Overlap : %s"), *OtherActor->GetName());
+	UE_LOG(LogActor, Warning, TEXT("Overlap : %s"), *OtherActor->GetActorNameOrLabel());
 
 	ITargetAble* targetAble = Cast<ITargetAble>(OtherActor);
 	
@@ -37,5 +68,10 @@ void UTargetingSystem::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActo
 	{
 		UE_LOG(LogActor, Warning, TEXT("Is TargetAble"));
 	}
+}
+
+void UTargetingSystem::OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	UE_LOG(LogActor, Warning, TEXT("overlap end : %s"), *OtherActor->GetActorNameOrLabel());
 }
 
--- Source/project_BnS/TargetingSystem.h
+++ Source/project_BnS/TargetingSystem.h
@@ -24,6 +24,8 @@ protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
+	virtual void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction) override;
+
 	// Targetting
 private:
 	UPROPERTY(VisibleAnywhere)
@@ -37,4 +39,7 @@ public:
 
 	UFUNCTION()
 	void OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnOverlapEnd(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 };
