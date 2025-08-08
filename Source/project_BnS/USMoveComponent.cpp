c653d582a7ab6dbb9a6e9c1d862a0056d359b449
Lowmin <lowmin28@hotmail.com>
2025-08-09 01:36:35 +0900

    벽타기 기본 로직 구현
    디테일 추가 필요

--- Source/project_BnS/USMoveComponent.cpp
+++ Source/project_BnS/USMoveComponent.cpp
@@ -1,304 +1,309 @@
- #include "USMoveComponent.h"
+#include "USMoveComponent.h"
 #include "Engine/Engine.h"
 #include "GameFramework/Character.h"
 #include "GameFramework/CharacterMovementComponent.h"
 #include "Kismet/KismetSystemLibrary.h"
+#include "DrawDebugHelpers.h"
 
 USMoveComponent::USMoveComponent()
 {
-    PrimaryComponentTick.bCanEverTick = true;
-    CurrentMoveState = EMoveState::Idle;
+	PrimaryComponentTick.bCanEverTick = true;
+	CurrentMoveState = EMoveState::Idle;
 }
 
 void USMoveComponent::BeginPlay()
 {
-    Super::BeginPlay();
-    MyPlayer = Cast<ACharacter>(GetOwner());
-    if (MyPlayer && MyPlayer->GetCharacterMovement())
-    {
-        MyPlayer->GetCharacterMovement()->MaxWalkSpeed = WalkSpeed;
-    }
+	Super::BeginPlay();
+	MyPlayer = Cast<ACharacter>(GetOwner());
+	if (MyPlayer && MyPlayer->GetCharacterMovement())
+	{
+		MyPlayer->GetCharacterMovement()->MaxWalkSpeed = WalkSpeed;
+	}
 }
 
 void USMoveComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
-    Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-    if (!MyPlayer || !MyPlayer->GetCharacterMovement())
-    {
-        return;
-    }
+	if (!MyPlayer || !MyPlayer->GetCharacterMovement())
+	{
+		return;
+	}
 
-    FVector curVelocity = MyPlayer->GetCharacterMovement()->Velocity;
+	FVector curVelocity = MyPlayer->GetCharacterMovement()->Velocity;
 
-    if (CurrentMoveState == EMoveState::Running)
-    {
-        FHitResult WallHit;
-        if (CheckWall(WallHit))
-        {
-            StartWallRun(WallHit);
-        }
-    }
+	if (CurrentMoveState == EMoveState::Running && MyPlayer->GetCharacterMovement()->IsMovingOnGround())
+	{
+		FHitResult WallHit;
+		if (CheckWall(WallHit))
+		{
+			StartWallRun(WallHit);
+		}
+	}
 
-    if (CurrentMoveState == EMoveState::Running && curVelocity.Size() <= 0)
-    {
-        SetMoveState(EMoveState::Idle);
-    }
+	if (CurrentMoveState == EMoveState::Running && curVelocity.Size() <= 0)
+	{
+		SetMoveState(EMoveState::Idle);
+	}
 
-    if (CurrentMoveState == EMoveState::WallRunning)
-    {
-        WallRun();
-    }
+	if (CurrentMoveState == EMoveState::WallRunning)
+	{
+		WallRun();
+	}
 
-    if (CurrentMoveState == EMoveState::Gliding)
-    {
-        Glide(DeltaTime);
-    }
+	if (CurrentMoveState == EMoveState::Gliding)
+	{
+		Glide(DeltaTime);
+	}
 
-    if (GEngine)
-    {
-        FString StateString = UEnum::GetValueAsString(CurrentMoveState);
-        GEngine->AddOnScreenDebugMessage(-1, 0.f, FColor::Yellow, StateString);
-    }
+	if (GEngine)
+	{
+		FString StateString = UEnum::GetValueAsString(CurrentMoveState);
+		GEngine->AddOnScreenDebugMessage(-1, 0.f, FColor::Yellow, FString::Printf(TEXT("MoveState: %s"), *StateString));
+	}
 }
 
 void USMoveComponent::SetMoveState(EMoveState NewState)
 {
-    if (CurrentMoveState != NewState)
-    {
-        CurrentMoveState = NewState;
-        SetMovementSpeed(NewState);
-    }
+	if (CurrentMoveState != NewState)
+	{
+		CurrentMoveState = NewState;
+		SetMovementSpeed(NewState);
+	}
 }
 
 EMoveState USMoveComponent::GetMoveState() const
 {
-    return CurrentMoveState;
+	return CurrentMoveState;
 }
 
 void USMoveComponent::SetMovementSpeed(EMoveState NewState)
 {
-    if (!MyPlayer || !MyPlayer->GetCharacterMovement()) return;
+	if (!MyPlayer || !MyPlayer->GetCharacterMovement()) return;
 
-    switch (NewState)
-    {
-    case EMoveState::Idle:
-        MyPlayer->GetCharacterMovement()->MaxWalkSpeed = WalkSpeed;
-        break;
-    case EMoveState::Running:
-        MyPlayer->GetCharacterMovement()->MaxWalkSpeed = RunSpeed;
-        break;
-    case EMoveState::WallRunning:
-        MyPlayer->GetCharacterMovement()->MaxWalkSpeed = WallRunSpeed;
-        break;
-    case EMoveState::Gliding:
-        MyPlayer->GetCharacterMovement()->MaxFlySpeed = GlideSpeed;
-        break;
-    default:
-        break;
-    }
+	switch (NewState)
+	{
+	case EMoveState::Idle:
+		MyPlayer->GetCharacterMovement()->MaxWalkSpeed = WalkSpeed;
+		break;
+	case EMoveState::Running:
+		MyPlayer->GetCharacterMovement()->MaxWalkSpeed = RunSpeed;
+		break;
+	case EMoveState::WallRunning:
+		break;
+	case EMoveState::Gliding:
+		MyPlayer->GetCharacterMovement()->MaxFlySpeed = GlideSpeed;
+		break;
+	default:
+		break;
+	}
 }
 
 bool USMoveComponent::CheckWall(FHitResult& OutHit)
 {
-    if (!MyPlayer || !GetWorld()) return false;
+	if (!MyPlayer || !GetWorld()) return false;
 
-    FVector Start = MyPlayer->GetActorLocation();
-    FVector End = Start + MyPlayer->GetActorForwardVector() * WallCheckDistance;
+	FVector Start = MyPlayer->GetActorLocation();
+	FVector End = Start + MyPlayer->GetActorForwardVector() * WallCheckDistance;
 
-    FCollisionQueryParams QueryParams;
-    QueryParams.AddIgnoredActor(MyPlayer);
+	FCollisionQueryParams QueryParams;
+	QueryParams.AddIgnoredActor(MyPlayer);
 
-    bool bHasHit = GetWorld()->LineTraceSingleByChannel(OutHit, Start, End, ECC_Visibility, QueryParams);
+	bool bHasHit = GetWorld()->LineTraceSingleByChannel(OutHit, Start, End, ECC_Visibility, QueryParams);
 
-    DrawDebugLine(
-        GetWorld(),
-        Start,
-        End,
-        bHasHit ? FColor::Green : FColor::Red,
-        false,
-        1.0f,
-        0,
-        1.0f
-    );
+	DrawDebugLine(
+		GetWorld(),
+		Start,
+		End,
+		bHasHit ? FColor::Green : FColor::Red,
+		false,
+		1.0f,
+		0,
+		1.0f
+	);
 
-    if (bHasHit)
-    {
-        if (OutHit.GetActor()->ActorHasTag("Wall"))
-        {
-            return true;
-        }
-    }
+	if (bHasHit)
+	{
+		if (OutHit.GetActor() && OutHit.GetActor()->ActorHasTag("Wall"))
+		{
+			return true;
+		}
+	}
 
-    return false;
+	return false;
 }
 
 void USMoveComponent::StartWallRun(const FHitResult& WallHit)
 {
-    if (!MyPlayer || !MyPlayer->GetCharacterMovement()) return;
+	if (!MyPlayer || !MyPlayer->GetCharacterMovement()) return;
 
-    SetMoveState(EMoveState::WallRunning);
-    MyPlayer->GetCharacterMovement()->GravityScale = 0.0f;
+	SetMoveState(EMoveState::WallRunning);
+	MyPlayer->GetCharacterMovement()->SetMovementMode(MOVE_Flying);
+	WallRunNormal = WallHit.ImpactNormal;
 }
 
 void USMoveComponent::StopWallRun()
 {
-    if (!MyPlayer || !MyPlayer->GetCharacterMovement()) return;
+	if (!MyPlayer || !MyPlayer->GetCharacterMovement()) return;
 
-    SetMoveState(EMoveState::Idle);
-    MyPlayer->GetCharacterMovement()->GravityScale = 1.0f;
+	SetMoveState(EMoveState::Idle);
+	MyPlayer->GetCharacterMovement()->SetMovementMode(MOVE_Walking);
 }
 
 void USMoveComponent::WallRun()
 {
+	if (!MyPlayer || !MyPlayer->GetCharacterMovement()) return;
+
+	FHitResult WallHit;
+	if (CheckWall(WallHit))
+	{
+		MyPlayer->GetCharacterMovement()->Velocity = FVector::UpVector * WallRunSpeed;
+	}
+	else
+	{
+		StopWallRun();
+	}
 }
 
 void USMoveComponent::SMoveToggle()
 {
-    if (!MyPlayer) return;
-    if (CurrentMoveState == EMoveState::Running)
-    {
-        StopSMove();
-    }
-    else if (CurrentMoveState == EMoveState::Idle)
-    {
-        StartSMove();
-    }
+	if (!MyPlayer) return;
+	if (CurrentMoveState == EMoveState::Running)
+	{
+		StopSMove();
+	}
+	else if (CurrentMoveState == EMoveState::Idle)
+	{
+		StartSMove();
+	}
 }
 
 void USMoveComponent::StartSMove()
 {
-    if (!MyPlayer || !MyPlayer->GetCharacterMovement()) return;
+	if (!MyPlayer || !MyPlayer->GetCharacterMovement()) return;
 
-    if (CurrentMoveState == EMoveState::Gliding)
-    {
-        StopGlide();
-    }
-    SetMoveState(EMoveState::Running);
+	if (CurrentMoveState == EMoveState::Gliding)
+	{
+		StopGlide();
+	}
+	SetMoveState(EMoveState::Running);
 }
 
 void USMoveComponent::StopSMove()
 {
-    if (!MyPlayer || !MyPlayer->GetCharacterMovement()) return;
+	if (!MyPlayer || !MyPlayer->GetCharacterMovement()) return;
 
-    SetMoveState(EMoveState::Idle);
+	SetMoveState(EMoveState::Idle);
 }
 
 void USMoveComponent::StartGlide()
 {
-    if (!MyPlayer || !MyPlayer->GetCharacterMovement()) return;
+	if (!MyPlayer || !MyPlayer->GetCharacterMovement()) return;
 
-    if (CurrentMoveState == EMoveState::Running)
-    {
-        StopSMove();
-    }
-    float groundDistance = CheckGroundDistance();
-    if (MyPlayer->GetCharacterMovement()->IsFalling() && groundDistance >= GlideMinHeight)
-    {
-        SetMoveState(EMoveState::Gliding);
-        MyPlayer->GetCharacterMovement()->SetMovementMode(MOVE_Flying);
-    }
+	if (CurrentMoveState == EMoveState::Running)
+	{
+		StopSMove();
+	}
+	float groundDistance = CheckGroundDistance();
+	if (MyPlayer->GetCharacterMovement()->IsFalling() && groundDistance >= GlideMinHeight)
+	{
+		SetMoveState(EMoveState::Gliding);
+		MyPlayer->GetCharacterMovement()->SetMovementMode(MOVE_Flying);
+		MyPlayer->GetCharacterMovement()->GravityScale = GlideGravityScale;
+	}
 }
 
 void USMoveComponent::StopGlide()
 {
-    if (!MyPlayer || !MyPlayer->GetCharacterMovement()) return;
+	if (!MyPlayer || !MyPlayer->GetCharacterMovement()) return;
 
-    SetMoveState(EMoveState::Running);
-    MyPlayer->GetCharacterMovement()->SetMovementMode(MOVE_Walking);
-    MyPlayer->GetCharacterMovement()->GravityScale = 1.0f;
+	SetMoveState(EMoveState::Running);
+	MyPlayer->GetCharacterMovement()->SetMovementMode(MOVE_Walking);
+	MyPlayer->GetCharacterMovement()->GravityScale = 1.0f;
 }
 
 void USMoveComponent::GlideToggle()
 {
-    if (!MyPlayer) return;
+	if (!MyPlayer) return;
 
-    if (CurrentMoveState != EMoveState::Gliding)
-    {
-        StartGlide();
-    }
-    else
-    {
-        StopGlide();
-    }
+	if (CurrentMoveState != EMoveState::Gliding)
+	{
+		StartGlide();
+	}
+	else
+	{
+		StopGlide();
+	}
 }
 void USMoveComponent::Glide(float DeltaTime)
 {
-	if (CurrentMoveState == EMoveState::Running)
-	{
-		const FVector Velocity = MyPlayer->GetCharacterMovement()->Velocity;
+	if (CurrentMoveState != EMoveState::Gliding) return;
 
-		if (MyPlayer->GetCharacterMovement()->IsMovingOnGround() && Velocity.SizeSquared() < 1.0f)
-		{
-			StopSMove();
-		}
-	}
-	if (CurrentMoveState != EMoveState::Running)
+	if (MyPlayer->GetCharacterMovement()->IsMovingOnGround())
 	{
-		if (CheckGroundDistance() <= 0.0f)
-		{
-			StopGlide();
-		}
-		else
-		{
-			MyPlayer->GetCharacterMovement()->AddInputVector(FVector(0, 0, -1) * 25.f * DeltaTime);
-		}
+		StopGlide();
+		return;
 	}
+
+	MyPlayer->GetCharacterMovement()->Velocity.Z = -GlideDescentSpeed;
 }
 
 void USMoveComponent::SJump()
 {
-    if (!MyPlayer || !MyPlayer->GetCharacterMovement()) return;
+	if (!MyPlayer || !MyPlayer->GetCharacterMovement()) return;
 
-    float CurrentJumpVelocity = JumpVelocity;
+	if (CurrentMoveState == EMoveState::WallRunning)
+	{
+		FVector LaunchVelocity = WallRunNormal * JumpVelocity + FVector(0.f, 0.f, JumpVelocity);
+		MyPlayer->LaunchCharacter(LaunchVelocity, false, true);
+		StopWallRun();
+		return;
+	}
 
-    if (MyPlayer->GetCharacterMovement()->IsFalling())
-    {
-        return;
-    }
+	if (MyPlayer->GetCharacterMovement()->IsFalling())
+	{
+		return;
+	}
 
-    if (CurrentMoveState == EMoveState::WallRunning)
-    {
-        StopWallRun();
-    }
+	float CurrentJumpVelocity = JumpVelocity;
 
-    if (CurrentMoveState == EMoveState::Running)
-    {
-        CurrentJumpVelocity = JumpVelocity * 2.f;
-    }
+	if (CurrentMoveState == EMoveState::Running)
+	{
+		CurrentJumpVelocity = JumpVelocity * 2.f;
+	}
 
-    if (CurrentMoveState == EMoveState::Gliding)
-    {
-        StopGlide();
-        return;
-    }
+	if (CurrentMoveState == EMoveState::Gliding)
+	{
+		StopGlide();
+		return;
+	}
 
-    MyPlayer->LaunchCharacter(FVector(0.f, 0.f, CurrentJumpVelocity), false, true);
+	MyPlayer->LaunchCharacter(FVector(0.f, 0.f, CurrentJumpVelocity), false, true);
 }
 
 float USMoveComponent::CheckGroundDistance()
 {
-    if (!MyPlayer || !GetWorld()) return -1.0f;
+	if (!MyPlayer || !GetWorld()) return -1.0f;
 
-    FHitResult HitResult;
-    FVector StartLocation = MyPlayer->GetActorLocation();
-    FVector EndLocation = StartLocation - FVector(0, 0, LineTraceLength);
-    StartLocation.Z += LineTraceStartOffset;
+	FHitResult HitResult;
+	FVector StartLocation = MyPlayer->GetActorLocation();
+	FVector EndLocation = StartLocation - FVector(0, 0, LineTraceLength);
+	StartLocation.Z += LineTraceStartOffset;
 
-    FCollisionQueryParams QueryParams;
-    QueryParams.AddIgnoredActor(MyPlayer);
+	FCollisionQueryParams QueryParams;
+	QueryParams.AddIgnoredActor(MyPlayer);
 
-    bool bHasHit = GetWorld()->LineTraceSingleByChannel(
-        HitResult,
-        StartLocation,
-        EndLocation,
-        ECollisionChannel::ECC_Visibility,
-        QueryParams
-    );
+	bool bHasHit = GetWorld()->LineTraceSingleByChannel(
+		HitResult,
+		StartLocation,
+		EndLocation,
+		ECollisionChannel::ECC_Visibility,
+		QueryParams
+	);
 
-    if (bHasHit)
-    {
-        return HitResult.Distance;
-    }
-    return -1.0f;
+	if (bHasHit)
+	{
+		return HitResult.Distance;
+	}
+	return -1.0f;
 }
--- Source/project_BnS/USMoveComponent.h
+++ Source/project_BnS/USMoveComponent.h
@@ -7,71 +7,72 @@
 UENUM(BlueprintType)
 enum class EMoveState : uint8
 {
-    Idle        UMETA(DisplayName = "Idle"),
-    Running     UMETA(DisplayName = "Running"),
-    Gliding     UMETA(DisplayName = "Gliding"),
-    WallRunning  UMETA(DisplayName = "WallRunning")
+	Idle		UMETA(DisplayName = "Idle"),
+	Running		UMETA(DisplayName = "Running"),
+	Gliding		UMETA(DisplayName = "Gliding"),
+	WallRunning	UMETA(DisplayName = "WallRunning")
 };
 
 UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class PROJECT_BNS_API USMoveComponent : public UActorComponent
 {
-    GENERATED_BODY()
+	GENERATED_BODY()
 
 public:
-    USMoveComponent();
+	USMoveComponent();
 
 protected:
-    virtual void BeginPlay() override;
-    virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+	virtual void BeginPlay() override;
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
 public:
-    void SetMoveState(EMoveState NewState);
-    EMoveState GetMoveState() const;
+	void SetMoveState(EMoveState NewState);
+	EMoveState GetMoveState() const;
 
-    // 경공(달리기)
-    void StartSMove();
-    void StopSMove();
-    void SMoveToggle();
+	// 경공(달리기)
+	void StartSMove();
+	void StopSMove();
+	void SMoveToggle();
 
-    // 글라이드
-    void StartGlide();
-    void StopGlide();
-    void GlideToggle();
-    void Glide(float DeltaTime);
+	// 글라이드
+	void StartGlide();
+	void StopGlide();
+	void GlideToggle();
+	void Glide(float DeltaTime);
 
-    // 벽타기
-    void StartWallRun(const FHitResult& WallHit);
-    void StopWallRun();
-    void WallRun(); 
+	// 벽타기
+	void StartWallRun(const FHitResult& WallHit);
+	void StopWallRun();
+	void WallRun();
 
-    // 점프
-    void SJump();
+	// 점프
+	void SJump();
 
 private:
-    void SetMovementSpeed(EMoveState NewState);
-    float CheckGroundDistance();
-    bool CheckWall(FHitResult& OutHit);
+	void SetMovementSpeed(EMoveState NewState);
+	float CheckGroundDistance();
+	bool CheckWall(FHitResult& OutHit);
 
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "SMove|State", meta = (AllowPrivateAccess = "true"))
-    EMoveState CurrentMoveState;
-    EMoveState PrevMoveState;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "SMove|State", meta = (AllowPrivateAccess = "true"))
+	EMoveState CurrentMoveState;
+	EMoveState PrevMoveState;
 
-    ACharacter* MyPlayer;
+	ACharacter* MyPlayer;
 
-    float WalkSpeed = 300.f;
-    float RunSpeed = 600.f;
-    float JumpVelocity = 600.f;
+	float WalkSpeed = 300.f;
+	float RunSpeed = 600.f;
+	float JumpVelocity = 600.f;
 
-    float GlideSpeed = 700.f;
-    float GlideGravityScale = 0.1f;
-    float GlideMinHeight = 0.0f;
-    float GlideDescentSpeed = 25.f;
+	float GlideSpeed = 700.f;
+	float GlideGravityScale = 0.1f;
+	float GlideMinHeight = 0.0f;
+	float GlideDescentSpeed = 25.f;
 
-    float LineTraceStartOffset = -100.f;
-    float LineTraceLength = 5000.f;
+	float LineTraceStartOffset = -100.f;
+	float LineTraceLength = 5000.f;
 
-    float WallRunSpeed = 600.f;
-    float WallCheckDistance = 100.f;
-    float WallRunZVelocity = 500.f;
+	float WallRunSpeed = 600.f;
+	float WallCheckDistance = 100.f;
+
+	FVector WallRunNormal;
 };
